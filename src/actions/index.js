1d96e04f31a9a08f0467ae6596cdf65a120ece13
AJLambda <apj2277@gmail.com>
2019-04-08 12:38:52 -0700

    created action packet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,27 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
 
 // Our action creators will return
 // an action packet that our reducer will
 // receive. What does the action packet look like?
+
 // Note that the action creator is not at all
 // responsible for handling any of the actual
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = () => {
+export const increment = count => {
   // Fill in this function
+  return {
+    type: INCREMENT,
+    payload: count + 1
+  };
 };
 
 export const decrement = () => {
   // Fill in this function
+  return {
+    type: DECREMENT,
+    payload: count + 1
+  };
 };
